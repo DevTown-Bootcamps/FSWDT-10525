5ee1e2730f32a5a4f29f4f8e15ee35186bbbd8be
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-04 19:41:13 +0530

    day-33

--- nodejs/day-29/day-33.js
+++ nodejs/day-29/day-33.js
@@ -62,6 +62,26 @@ app.post("/api/register",async(req,res)=>{
    }
 })
 
+//login
+app.post("/api/login",async(req,res)=>{
+  const {username,password}=req.body;
+
+  try{
+   const user=await User.findOne({username});
+
+   if(!user) return res.status(404).json({messgae:"The user that you are trying to search does not exist"});
+
+   const isMatch=await bcrypt.compare(password,user.password);
+
+   if(!isMatch) return res.status(400).json({messgae:"Password did not match"});
+   else{
+    res.status(200).json({message:"Login success"});
+   }
+  }catch(err){
+
+  }
+})
+
 //Get all todos
 app.get('/api/todos',async(req,res)=>{
     try{
